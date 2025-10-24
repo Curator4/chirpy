d0f4eb9b210f689c489fb9c44142d8b70f9854e0
curator <curator4@gmail.com>
2025-10-24 14:25:45 +0200

    ch2l1

--- main.go
+++ main.go
@@ -1,11 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 const port = "8080"
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metrics(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hits: %d\n", cfg.fileserverHits.Load())
+}
+
+func (cfg *apiConfig) reset(w http.ResponseWriter, _ *http.Request) {
+	cfg.fileserverHits.Store(int32(0))
+	w.WriteHeader(http.StatusOK)
+}
 
 func main() {
 	mux := http.NewServeMux()
@@ -13,16 +37,21 @@ func main() {
 		Addr:    ":" + port,
 		Handler: mux,
 	}
+	apiCfg := apiConfig{}
+	fs := http.FileServer(http.Dir("."))
+	app := http.StripPrefix("/app", fs)
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(app))
 	mux.HandleFunc("/healthz", ready)
+	mux.HandleFunc("/metrics", apiCfg.metrics)
+	mux.HandleFunc("/reset", apiCfg.reset)
 
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatalf("server error %v", err)
 	}
 }
 
-func ready(w http.ResponseWriter, r *http.Request) {
+func ready(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
