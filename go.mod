e80971945224f5e6bc3518c35da8af309bcf307b
curator <curator4@gmail.com>
2025-10-24 10:54:12 +0200

    ch1l4

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Curator4/chirpy
+
+go 1.25.3
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatalf("server error %v", err)
+	}
+}
